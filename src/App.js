46b43cb5c6d91eacc5942fbdb17bd118c9c7cd53
Kory Newton <korybnewton@gmail.com>
2019-11-07 20:40:15 -0800

    front end initialized, connects to socket server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
-function App() {
-  return <div>hello</div>;
+const { REACT_APP_SERVER_ADDR: socketAddress } = process.env;
+
+const client = new W3CWebSocket(`ws://${socketAddress}`);
+
+class App extends Component {
+  state = {
+    id: ''
+  };
+  componentWillMount() {
+    client.onopen = message => {
+      console.log('WebSocket Client Connected');
+      console.log(message);
+    };
+    client.onmessage = message => {
+      console.log('message from server');
+      console.log(message);
+    };
+  }
+
+  test = () => {
+    console.log('test called:');
+    const { data } = this.state;
+    client.send('hello from client');
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Websockets</h2>
+        <button onClick={this.test}>CLICK</button>
+      </div>
+    );
+  }
 }
 
 export default App;
