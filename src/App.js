d47b5b8ac8b3c9595f7dba6af0e378740fe2c8e7
Kory Newton <korybnewton@gmail.com>
2019-11-09 19:50:04 -0800

    plays sound on new message from server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import Sound from 'react-sound';
+
+import BubblePop from './sounds/bubble_pop.mp3';
 
 const { REACT_APP_SERVER_ADDR: socketAddress } = process.env;
 
@@ -7,7 +10,8 @@ const client = new W3CWebSocket(`ws://${socketAddress}`);
 
 class App extends Component {
   state = {
-    id: ''
+    id: '',
+    status: 'STOPPED'
   };
   componentWillMount() {
     client.onopen = message => {
@@ -15,22 +19,20 @@ class App extends Component {
       console.log(message);
     };
     client.onmessage = message => {
-      console.log('message from server');
-      console.log(message);
+      this.setState({ status: 'PLAYING' });
     };
   }
 
-  test = () => {
-    console.log('test called:');
-    const { data } = this.state;
-    client.send('hello from client');
-  };
-
   render() {
+    const { status } = this.state;
     return (
       <div>
         <h2>Websockets</h2>
-        <button onClick={this.test}>CLICK</button>
+        <Sound
+          url={BubblePop}
+          playStatus={Sound.status[`${status}`]}
+          onFinishedPlaying={() => this.setState({ status: 'STOPPED' })}
+        />
       </div>
     );
   }
